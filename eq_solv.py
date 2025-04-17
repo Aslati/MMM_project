57c23cf1149f104207baaeec56493c2ba33c6017
Aslati <joajabcz@gmail.com>
2025-04-17 12:01:21 +0200

    Implemented .zip download, fixed derivatives functions, added setting params

--- eq_solv.py
+++ eq_solv.py
@@ -7,30 +7,56 @@ import streamlit as sl
 import pandas as pd
 import time
 
+#params [J1, J2, n1, n2, k, b, T]
+params = []
+def dx1(x1,x2): #pochodna y
+    return x2
+
+def dx2(x1,x2):
+    return (params[6][0]*params[3]/params[2]-params[4]*x1-params[5]*x2)/(params[0]*np.power(params[3]/params[2], 2)+params[1])
+
 def Euler(time_axis):
-    'Starting a long computation...'
-
-    # Add a placeholder
-    # latest_iteration = sl.empty()
-    # bar = sl.progress(0)
-
-    # for i in range(100):
-    #     # Update the progress bar with each iteration.
-    #     latest_iteration.text(f'Iteration {i+1}')
-    #     bar.progress(i + 1)
-    #     time.sleep(0.1)
-
-    # '...and now we\'re done!'
-    # print("Euler")
+    
     some_data = []
     rng = np.random.default_rng()
     for i in range(3):
         some_data.append(rng.random()*5)
+        print(some_data)
     chart_data = pd.DataFrame(
-        some_data, time_axis
+        {"output": some_data}, time_axis
     )
     sl.line_chart(chart_data)
+    print(chart_data)
+    return chart_data
 
-def Runge_Kutta():
-    print("Runge-Kutta")
-    return  0.01
+def Runge_Kutta(paramsRK, x, y):
+    params=paramsRK
+    h = 0.1
+    for n in range(100):
+        x.append(x[n] + h)
+        k1 = h*f(x[n], y[n])
+        k2 = h*f(x[n]+h/2, y[n]+k1/2)
+        k3 = h*f(x[n]+h/2, y[n]+k2/2)
+        k4 = h*f(x[n]+h, y[n]+k3)
+        
+        dyn = 1/6*(k1+2*k2+2*k3+k4)
+        y.append(y[n] + dyn)
+        chart_data = pd.DataFrame(
+            y, x
+        )
+    return chart_data 
+
+def waiting():
+    'Starting a long computation...'
+
+   # Add a placeholder
+    latest_iteration = sl.empty()
+    bar = sl.progress(0)
+
+    for i in range(100):
+        # Update the progress bar with each iteration.
+        latest_iteration.text(f'Iteration {i+1}')
+        bar.progress(i + 1)
+        time.sleep(0.1)
+
+    '...and now we\'re done!'
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 import matplotlib.pyplot as plt
 import streamlit as sl
@@ -5,27 +6,77 @@ import pandas as pd
 import control as cl
 import scipy as sc
 import control.matlab as cmtl
-from eq_solv import Euler, Runge_Kutta
+import zipfile
+from eq_solv import Euler, Runge_Kutta, f
+
+sl.set_page_config("Project")
 
 time_axis = [1, 2, 3] 
 
-
-## sl.write(plt.plot(time_axis, some_data))
 sidebar_frame=sl.sidebar
+
+sidebar_frame.image('image.png')
+
+J1 = sidebar_frame.text_input("J1:")
+J2 = sidebar_frame.text_input("J2:")
+n1 = sidebar_frame.text_input("n1:")
+n2 = sidebar_frame.text_input("n2:")
+k = sidebar_frame.text_input("k:")
+b = sidebar_frame.text_input("b:")
+T = []
+
 signal_type = sidebar_frame.selectbox("Sygnał", ['trójkątny', 'prostokątny', 'harmoniczny'])
 #sidebar_frame_form = sl.sidebar.form("Własności sygnału")
 
+x = [0]
+y = [0]
 
+# Runge_Kutta(x, y,)
+
+# chart = pd.DataFrame(
+#     Runge_Kutta(x,y,f)[1], Runge_Kutta(x,y,f)[0]
+# )
+
+# #sl.line_chart(chart)
 
 match signal_type:
     case 'prostokątny':
-        sidebar_frame.slider("Wypełnienie:", 1, 100)
-        sidebar_frame.button("Symuluj")
+        fill = sidebar_frame.slider("Wypełnienie:", 1, 100)
     case 'harmoniczny':
-        sidebar_frame.checkbox("Wypełnienie 100%")
-        sidebar_frame.button("Symuluj", on_click=Euler(time_axis)) #to be fixed
-##on_click=Euler(chart_data)        
+        fill = sidebar_frame.slider("Wypełnienie:", 1, 100)
+
+T = []
+
+euler_data=pd.DataFrame()
+rk_data = pd.DataFrame()
 
 
+def csv_for_download(data, name):
+    file = data.to_csv().encode("utf-8")
+    data.to_csv(name, index=None, sep=",", header=True, encoding='utf-8-sig')
+    print("done")
+    return file
+
+if sidebar_frame.button("Symuluj"):
+    params = [J1, J2, n1, n2, k, b, T]
+    euler_data = Euler(time_axis)
+    rk_data = Runge_Kutta(params, x, y)
+
+    euler_csv = csv_for_download(euler_data, 'euler_data.csv')
+    rk_csv = csv_for_download(rk_data, 'runge_kutter_data.csv')
+
+    with zipfile.ZipFile('data.zip', 'w') as zf:
+        zf.write('euler_data.csv')
+        zf.write('runge_kutter_data.csv')
+
+    os.remove('runge_kutter_data.csv')
+    os.remove('euler_data.csv')
+    with open("data.zip", "rb") as fp:
+        sl.download_button(
+            label = "Download CSV",
+            data = fp,
+            file_name="data.zip",
+            mime="application/zip"
+        ) 
     
-
+    os.remove('data.zip')
